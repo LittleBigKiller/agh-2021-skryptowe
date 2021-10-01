e6556d90554890dbd5bcaa300a1d7287ff94c0d8
LittleBigKiller <35974496+LittleBigKiller@users.noreply.github.com>
2021-10-01 14:37:53 +0200

    [Zaj06] Komentarze do jednolinijkowca

--- Zaj06/zad2-1.py
+++ Zaj06/zad2-1.py
@@ -1,1 +1,25 @@
+import sys
+from collections import Counter
+
+# wczytaj dane z stdin
+sys.stdin.read()
+
+# rozbij na listę pojedynczych słów
+sys.stdin.read().split()
+
+# dla x podaj jego długość (jeśli string/lista)
+lambda x: len(x)
+
+# na każdym elemencie listy słów wykonaj powyższą funkcję
+map(lambda x: len(x), sys.stdin.read().split())
+
+# policz ile jest elementów o jakiej wartości
+Counter(map(lambda x: len(x), sys.stdin.read().split()))
+
+# zamień obiekt counter na siłę na słownik
+dict(Counter(map(lambda x: len(x), sys.stdin.read().split())))
+
+# gotowa funkcja:
+'''
 import sys; from collections import Counter; print(dict(Counter(map(lambda x: len(x), sys.stdin.read().split()))))
+'''
