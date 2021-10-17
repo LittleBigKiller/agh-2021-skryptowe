ff8c5e9116df59df945e11981e470895e6ce79a2
LittleBigKiller <35974496+LittleBigKiller@users.noreply.github.com>
2021-10-17 20:17:02 +0200

    [Zaj02] Lepsze testy, regex nie trafia whitespace

--- Zaj02/skrypt.py
+++ Zaj02/skrypt.py
@@ -1,33 +1,42 @@
 import re
 
 def match_word(string):
-    pat_letters = re.compile(r'\D+')
+    pat_letters = re.compile(r'[^\d|^\s]+')
     res_letters = pat_letters.search(string)
     
-    return (res_letters.start(), res_letters.group())
+    if res_letters:
+        return (res_letters.start(), res_letters.group())
+    else:
+        return None
 
 def match_number(string):
     pat_numbers = re.compile(r'\d+')
     res_numbers = pat_numbers.search(string)
     
-    return (res_numbers.start(), res_numbers.group())
+    if res_numbers:
+        return (res_numbers.start(), res_numbers.group())
+    else:
+        return None
 
 
 
 if __name__ == '__main__':
-    test_string = input()
-
-    word_res = match_word(test_string)        
-    number_res = match_number(test_string)
-    
-    if number_res and word_res:
-        if number_res < word_res:
-            print('Liczba: {}'.format(number_res[1]))
-            print('Wyraz: {}'.format(word_res[1]))
-        else:
-            print('Wyraz: {}'.format(word_res[1]))
-            print('Liczba: {}'.format(number_res[1]))
-    elif word_res:
-        print('Wyraz: {}'.format(word_res[1]))
-    elif number_res:
-        print('Liczba: {}'.format(number_res[1]))
+    while True:
+        try:
+            test_string = input()
+            word_res = match_word(test_string)        
+            number_res = match_number(test_string)
+            
+            if number_res and word_res:
+                if number_res < word_res:
+                    print('Liczba: {}'.format(number_res[1]))
+                    print('Wyraz: {}'.format(word_res[1]))
+                else:
+                    print('Wyraz: {}'.format(word_res[1]))
+                    print('Liczba: {}'.format(number_res[1]))
+            elif word_res:
+                print('Wyraz: {}'.format(word_res[1]))
+            elif number_res:
+                print('Liczba: {}'.format(number_res[1]))
+        except:
+            break
--- Zaj02/test.py
+++ Zaj02/test.py
@@ -15,6 +15,30 @@ class Test_TestIncrementDecrement(unittest.TestCase):
     def test_number_start(self):
         self.assertEqual(skrypt.match_number("1231Ala")[0], 0)
 
+    def test_just_word_match(self):
+        self.assertEqual(skrypt.match_word("Ala")[1], "Ala")
+
+    def test_just_word_start(self):
+        self.assertEqual(skrypt.match_word("Ala")[0], 0)
+
+    def test_just_number_match(self):
+        self.assertEqual(skrypt.match_number("1231")[1], "1231")
+
+    def test_just_number_start(self):
+        self.assertEqual(skrypt.match_number("1231")[0], 0)
+
+    def test_leading_space_word_match(self):
+        self.assertEqual(skrypt.match_word("    1231Ala")[1], "Ala")
+
+    def test_leading_space_word_start(self):
+        self.assertEqual(skrypt.match_word("    1231Ala")[0], 8)
+
+    def test_leading_space_number_match(self):
+        self.assertEqual(skrypt.match_number("    1231Ala")[1], "1231")
+
+    def test_leading_space_number_start(self):
+        self.assertEqual(skrypt.match_number("    1231Ala")[0], 4)
+
 
 if __name__ == '__main__':
     unittest.main()
