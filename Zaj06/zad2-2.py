a2e4b3d343c202a8430fb4860683f0311e815ef9
LittleBigKiller <35974496+LittleBigKiller@users.noreply.github.com>
2021-11-24 17:09:05 +0100

    [Zaj06] Sortowanie kluczy

--- Zaj06/zad2-2.py
+++ Zaj06/zad2-2.py
@@ -1,28 +1,28 @@
-'''
 import sys
 from collections import Counter
 
-# wczytaj dane z stdin
-sys.stdin.read()
 
-# rozbij na listę pojedynczych słów
-sys.stdin.read().split()
+# wypisujemy na ekran
+print(
+    # tworzymy z krotki słownik
+    dict(
+        # sortujemy tablicę krotek
+        sorted(
+            # korzystamy z licznika i wyciągamy z niego krotki (długość, ilość)
+            Counter(
+                # dla każdego słowa w tablicy wykonujemy funkcję
+                map(
+                    # funkcja licząca długość podanego stringa
+                    lambda x: len(x),
+                    # wczytujemy dane z konsoli i rodzielamy je na słowa
+                    sys.stdin.read().split()
+                )
+            ).items()
+        )
+    )
+)
 
-# dla x podaj jego długość (jeśli string/lista)
-lambda x: len(x)
-
-# na każdym elemencie listy słów wykonaj powyższą funkcję
-map(lambda x: len(x), sys.stdin.read().split())
-
-# policz ile jest elementów o jakiej wartości
-Counter(map(lambda x: len(x), sys.stdin.read().split()))
-
-# zamień obiekt counter na siłę na słownik
-dict(Counter(map(lambda x: len(x), sys.stdin.read().split())))
-
-# gotowa funkcja:
-print(dict(Counter(map(lambda x: len(x), sys.stdin.read().split()))))
 '''
-
 # oneliner:
-import sys; from collections import Counter; print(dict(Counter(map(lambda x: len(x), sys.stdin.read().split()))))
+import sys; from collections import Counter; print(dict(sorted(Counter(map(lambda x: len(x), sys.stdin.read().split())).items())))
+'''
