da84a05ec09fc78902a4e70e3ad37c44c33b0505
LittleBigKiller <35974496+LittleBigKiller@users.noreply.github.com>
2022-01-13 23:49:34 +0100

    [Zaj 11] Porządek w testach

--- Zaj11/test/test.js
+++ Zaj11/test/test.js
@@ -25,163 +25,172 @@ function getRandomInt(max) {
 var server = supertest.agent("http://localhost:3000");
 
 // UNIT test begin
-describe('GET /', function () {
-  it('respond with html', function (done) {
-    server
-      .get('/')
-      .expect('Content-Type', /html/)
-      .expect(200, done);
+describe('Zaj11', function () {
+  describe('Zad1', function () {
+    describe('GET /', function () {
+      it('respond with html', function (done) {
+        server
+          .get('/')
+          .expect('Content-Type', /html/)
+          .expect(200, done);
+      });
+
+      it('contain "3" in h1', function (done) {
+        function testValue(res) {
+          if (!(res.text.match(/(?<=<h1>)[0-9]+(?=<\/h1>)/gm)[0] == 3))
+            throw new Error('response doesn\'t match')
+        }
+
+        server
+          .get('/')
+          .expect('Content-Type', /html/)
+          .expect(200)
+          .expect(testValue)
+          .end(done)
+      });
+    });
   });
 
-  it('contain "3" in h1', function (done) {
-    function testValue(res) {
-      if (!(res.text.match(/(?<=<h1>)[0-9]+(?=<\/h1>)/gm)[0] == 3))
-        throw new Error('response doesn\'t match')
-    }
+  describe('Zad2', function () {
+    describe('Check example.json', function () {
+      it('is a json file', function (done) {
+        expect('./Zaj11/example.json').to.be.a.jsonFile();
+        done()
+      });
 
-    server
-      .get('/')
-      .expect('Content-Type', /html/)
-      .expect(200)
-      .expect(testValue)
-      .end(done)
+      it('contains at least one element matching schema', function (done) {
+        expect('./Zaj11/example.json').to.be.a.jsonFile().and.contain.jsonWithProps({
+          "x": 4,
+          "y": 2,
+          "op": "+"
+        })
+        expect('./Zaj11/example.json').to.be.a.jsonFile().and.contain.jsonWithProps({
+          "x": 20,
+          "y": 4,
+          "op": "+"
+        })
+        expect('./Zaj11/example.json').to.be.a.jsonFile().and.contain.jsonWithProps({
+          "x": 4,
+          "y": 2,
+          "op": "-"
+        })
+        expect('./Zaj11/example.json').to.be.a.jsonFile().and.contain.jsonWithProps({
+          "x": 20,
+          "y": 4,
+          "op": "-"
+        })
+        expect('./Zaj11/example.json').to.be.a.jsonFile().and.contain.jsonWithProps({
+          "x": 4,
+          "y": 2,
+          "op": "*"
+        })
+        expect('./Zaj11/example.json').to.be.a.jsonFile().and.contain.jsonWithProps({
+          "x": 20,
+          "y": 4,
+          "op": "*"
+        })
+        expect('./Zaj11/example.json').to.be.a.jsonFile().and.contain.jsonWithProps({
+          "x": 4,
+          "y": 2,
+          "op": "/"
+        })
+        expect('./Zaj11/example.json').to.be.a.jsonFile().and.contain.jsonWithProps({
+          "x": 20,
+          "y": 4,
+          "op": "/"
+        })
+        done()
+      })
+    })
+
+    describe('GET /json/:name (using example.json)', function () {
+      it('respond with html', function (done) {
+        server
+          .get('/json/example.json')
+          .expect('Content-Type', /html/)
+          .expect(200, done);
+      });
+
+      it('contain correct value for all operations', function (done) {
+        server
+          .get('/json/example.json')
+          .expect('Content-Type', /html/)
+          .expect(200)
+          .expect((res) => {
+            var match = res.text.match(/(?<=<tr>).*?(?=<\/tr>)/gm)
+            for (line of match) {
+              var lm = line.match(/(?<=<td>).*?(?=<\/td>)/gm)
+              if (lm !== null) {
+                assert.equal(eval(lm[0] + lm[1] + lm[2]), lm[3])
+              }
+            }
+
+          })
+          .end(done)
+      })
+    })
+  });
+
+  describe('Zad3', function () {
+
+    describe('GET /calculate/:operation/:x/:y (using example.json)', function () {
+      it('respond with html', function (done) {
+        server
+          .get('/calculate/+/1/1')
+          .expect('Content-Type', /html/)
+          .expect(200, done);
+      });
+
+      it('check for invalid operations', function (done) {
+        server
+          .get('/calculate/x/1/1')
+          .expect('Content-Type', /html/)
+          .expect(400)
+          .end(done)
+      })
+
+      it('save valid operation to database', function (done) {
+        var x = getRandomInt(65535)
+        var y = getRandomInt(65535)
+        server
+          .get('/calculate/-/' + x + '/' + y)
+          .expect('Content-Type', /html/)
+          .expect(200)
+          .expect(async (res) => {
+            var json = await Op.find({ x: x, y: y, op: '-' });
+
+            console.log(json.length)
+
+            expect(json.length).to.be.greaterThanOrEqual(1)
+          })
+          .end(done)
+      })
+    })
+
+    describe('GET /results', function () {
+      it('respond with html', function (done) {
+        server
+          .get('/results')
+          .expect('Content-Type', /html/)
+          .expect(200, done);
+      });
+
+      it('contain correct value for all operations', function (done) {
+        server
+          .get('/results')
+          .expect('Content-Type', /html/)
+          .expect(200)
+          .expect(async (res) => {
+            var match = res.text.match(/(?<=<tr>).*?(?=<\/tr>)/gm)
+            for (line of match) {
+              var lm = line.match(/(?<=<td>).*?(?=<\/td>)/gm)
+              if (lm !== null) {
+                assert.equal(eval(lm[0] + lm[1] + lm[2]), lm[3])
+              }
+            }
+          })
+          .end(done)
+      })
+    })
   });
 });
-
-describe('Check example.json', function () {
-  it('is a json file', function (done) {
-    expect('./Zaj11/example.json').to.be.a.jsonFile();
-    done()
-  });
-
-  it('contains at least one element matching schema', function (done) {
-    expect('./Zaj11/example.json').to.be.a.jsonFile().and.contain.jsonWithProps({
-      "x": 4,
-      "y": 2,
-      "op": "+"
-    })
-    expect('./Zaj11/example.json').to.be.a.jsonFile().and.contain.jsonWithProps({
-      "x": 20,
-      "y": 4,
-      "op": "+"
-    })
-    expect('./Zaj11/example.json').to.be.a.jsonFile().and.contain.jsonWithProps({
-      "x": 4,
-      "y": 2,
-      "op": "-"
-    })
-    expect('./Zaj11/example.json').to.be.a.jsonFile().and.contain.jsonWithProps({
-      "x": 20,
-      "y": 4,
-      "op": "-"
-    })
-    expect('./Zaj11/example.json').to.be.a.jsonFile().and.contain.jsonWithProps({
-      "x": 4,
-      "y": 2,
-      "op": "*"
-    })
-    expect('./Zaj11/example.json').to.be.a.jsonFile().and.contain.jsonWithProps({
-      "x": 20,
-      "y": 4,
-      "op": "*"
-    })
-    expect('./Zaj11/example.json').to.be.a.jsonFile().and.contain.jsonWithProps({
-      "x": 4,
-      "y": 2,
-      "op": "/"
-    })
-    expect('./Zaj11/example.json').to.be.a.jsonFile().and.contain.jsonWithProps({
-      "x": 20,
-      "y": 4,
-      "op": "/"
-    })
-    done()
-  })
-})
-
-describe('GET /json/:name (using example.json)', function () {
-  it('respond with html', function (done) {
-    server
-      .get('/json/example.json')
-      .expect('Content-Type', /html/)
-      .expect(200, done);
-  });
-
-  it('contain correct value for all operations', function (done) {
-    server
-      .get('/json/example.json')
-      .expect('Content-Type', /html/)
-      .expect(200)
-      .expect((res) => {
-        var match = res.text.match(/(?<=<tr>).*?(?=<\/tr>)/gm)
-        for (line of match) {
-          var lm = line.match(/(?<=<td>).*?(?=<\/td>)/gm)
-          if (lm !== null) {
-            assert.equal(eval(lm[0] + lm[1] + lm[2]), lm[3])
-          }
-        }
-
-      })
-      .end(done)
-  })
-})
-
-describe('GET /calculate/:operation/:x/:y (using example.json)', function () {
-  it('respond with html', function (done) {
-    server
-      .get('/calculate/+/1/1')
-      .expect('Content-Type', /html/)
-      .expect(200, done);
-  });
-
-  it('check for invalid operations', function (done) {
-    server
-      .get('/calculate/x/1/1')
-      .expect('Content-Type', /html/)
-      .expect(400)
-      .end(done)
-  })
-
-  it('save valid operation to database', function (done) {
-    var x = getRandomInt(65535)
-    var y = getRandomInt(65535)
-    server
-      .get('/calculate/-/' + x + '/' + y)
-      .expect('Content-Type', /html/)
-      .expect(200)
-      .expect(async (res) => {
-        var json = await Op.find({ x: x, y: y, op: '-' });
-
-        console.log(json.length)
-
-        expect(json.length).to.be.greaterThanOrEqual(1)
-      })
-      .end(done)
-  })
-})
-
-describe('GET /results', function () {
-  it('respond with html', function (done) {
-    server
-      .get('/results')
-      .expect('Content-Type', /html/)
-      .expect(200, done);
-  });
-
-  it('contain correct value for all operations', function (done) {
-    server
-      .get('/results')
-      .expect('Content-Type', /html/)
-      .expect(200)
-      .expect(async (res) => {
-        var match = res.text.match(/(?<=<tr>).*?(?=<\/tr>)/gm)
-        for (line of match) {
-          var lm = line.match(/(?<=<td>).*?(?=<\/td>)/gm)
-          if (lm !== null) {
-            assert.equal(eval(lm[0] + lm[1] + lm[2]), lm[3])
-          }
-        }
-      })
-      .end(done)
-  })
-})
